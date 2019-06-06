4267325fe2b5b29a47bce864a41adbf0ada10bbc
ericjosprice <epriceutexas@gmail.com>
2019-06-06 14:16:11 -0500

    added new breakpoint for larger view port

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,26 @@
+// $('.slider').slick({
+//     centerMode: true,
+//     centerPadding: '50px',
+//     slidesToShow: 3,
+//     arrows:true,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '30px',
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: true,
+//           centerMode: true,
+//           centerPadding: '10px',
+//           slidesToShow: 3
+//         }
+//       }
+//     ]
+//   });
